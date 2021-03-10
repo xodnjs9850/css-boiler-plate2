dfbabd25f78d7c11d9b1a23b32511ee876859b56
xodnjs9850 <xodnjs9850@naver.com>
2021-03-10 23:49:36 +0900

    수정 완료

--- js/scroll-event.js
+++ js/scroll-event.js
@@ -1,40 +1,51 @@
 
 
-function hideGoTop(ScrollTop) {
+//맨 위로 이동 버튼 나타내고 숨겨주는 함수
+function hideGoTop(scrollTop) {
     const $goTop = document.querySelector('.go-top');
-    if (ScrollTop < 800) {
+
+    $goTop.classList.toggle('hide', scrollTop < 800);
+
+    /*
+    if (scrollTop < 800) {
         $goTop.classList.add('hide');
     } else {
         $goTop.classList.remove('hide');
     }
-    
-    // $goTop.classList.toggle('hide', ScrollTop < 800);
+    */
 }
 
-function changeFixHeader(ScrollTop) {
-    const $logoImg = document.querySelector('inner-header h1 img');
-    if (ScrollTop > 200) {
+//스크롤을 일정부분 내리면 헤더의 css와 로고이미지를 교체하는 함수
+function changeFixHeader(scrollTop) {
+
+
+    const $logoImg = document.querySelector('.inner-header h1 img');
+    if (scrollTop > 200) {
         $logoImg.setAttribute('src', 'img/logo2.png');
         $('header').addClass('on');
+        // document.querySelector('header').classList.add('on');
     } else {
         $logoImg.setAttribute('src', 'img/logo.png');
         $('header').removeClass('on');
     }
-    
 }
 
-
 //즉시 실행 함수
-(function () {
+(function(){
 
-    //scroll 이벤트 생성 - 1. 헤더 변경 2. 맨위로 이동 화살표 처리
+    //스크롤 이벤트 - 1. 헤더 변경 2. 맨위로 이동 화살표 처리
     document.addEventListener('scroll', e => {
-        // console.log('스크롤 이벤트 발생');
-        // console.log(document.documentElement.scrollTop);
+        // console.log('스크롤 이벤트 발생!');
 
-        const ScrollTop = document.documentElement.scrollTop;
-        // 맨위로 이동 버튼 처리
-        hideGoTop(ScrollTop);
-        changeFixHeader(ScrollTop);
+        //스크롤 좌표값 얻는 방법
+        // console.log(document.documentElement.scrollTop);
+        const scrollTop = document.documentElement.scrollTop;
+
+        //맨위로 이동 버튼 처리
+        hideGoTop(scrollTop);
+        //헤더 변경 처리
+        changeFixHeader(scrollTop);
+        
     });
+
 }());
